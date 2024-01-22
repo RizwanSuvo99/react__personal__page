fa96121c622e5e7ca05bcbf6fd7cb61a03f29563
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-22 12:15:49 +0600

    "Personal Page created"

---
+++ src/pages/Personal/Personal.jsx
@@ -0,0 +1,10 @@
+import { personal } from "./Personal.module.css";
+const Personal = () => {
+  return (
+    <section className={personal}>
+      
+    </section>
+  );
+};
+
+export default Personal;
---
+++ src/utils/imageData.js
@@ -0,0 +1,122 @@
+export const imageGallery = [
+  {
+    imgUrl: "https://example.com/image1.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image2.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image3.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image4.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image5.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image6.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image7.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image8.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image9.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image10.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image11.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image12.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image13.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image14.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image15.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image16.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image17.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image18.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image19.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image20.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image21.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image22.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image23.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image24.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image25.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image26.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image27.jpg",
+    yearClicked: 2012,
+  },
+  {
+    imgUrl: "https://example.com/image28.jpg",
+    yearClicked: 2014,
+  },
+  {
+    imgUrl: "https://example.com/image29.jpg",
+    yearClicked: 2013,
+  },
+  {
+    imgUrl: "https://example.com/image30.jpg",
+    yearClicked: 2012,
+  },
+];
