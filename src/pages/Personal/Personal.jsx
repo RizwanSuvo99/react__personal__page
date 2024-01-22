759565857db252f70c39e4334626fd6011a9556a
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-22 17:14:52 +0600

    "Grid gallery updated"

--- src/pages/Personal/Personal.jsx
+++ src/pages/Personal/Personal.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { imageGallery } from "../../utils/imageData";
 import {
   big__img,
@@ -5,25 +6,58 @@ import {
   personal,
   personal__img,
   personal__timeline,
+  vertical__img,
+  active
 } from "./Personal.module.css";
+
 const Personal = () => {
+  const [isYear, setIsYear] = useState(0);
+
   const imgData = imageGallery;
+  const yearArr = [];
+  imageGallery.map((img) => {
+    if (!yearArr.includes(img.yearClicked)) {
+      yearArr.push(img.yearClicked);
+    }
+  });
+
+  const bgImg = {
+    width: "100%",
+    minHeight: "100vh",
+    backgroundImage: `url(${imgData[0].imgUrl})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   console.log(imgData);
   return (
-    <section className={personal}>
+    <section className={personal} style={bgImg}>
       <div className={personal__img}>
         {imgData.map((img) => (
           <div
             key={img.id}
             className={
-              img.id % 3 == 0 || img.id % 4 == 0 ? big__img : horizontal__img
+              img.id % 3 == 0 || img.id % 4 == 0
+                ? big__img
+                : img.id % 5 == 0 || img.id % 7
+                ? vertical__img
+                : horizontal__img
             }
           >
             <img src={img.imgUrl} alt="image" />
           </div>
         ))}
       </div>
-      <div className={personal__timeline}></div>
+      <div className={personal__timeline}>
+        <ul>
+          {yearArr.map((year) => (
+            <li key={year} className={isYear && "active"}>
+              {year}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -49,104 +49,104 @@ export const imageGallery = [
     imgUrl: "https://picsum.photos/200/300?random=10",
     yearClicked: 2014,
   },
-  // {
-  //   id: 11,
-  //   imgUrl: "https://picsum.photos/200/300?random=11",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 12,
-  //   imgUrl: "https://picsum.photos/200/300?random=12",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 13,
-  //   imgUrl: "https://picsum.photos/200/300?random=13",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 14,
-  //   imgUrl: "https://picsum.photos/200/300?random=14",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 15,
-  //   imgUrl: "https://picsum.photos/200/300?random=15",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 16,
-  //   imgUrl: "https://picsum.photos/200/300?random=16",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 17,
-  //   imgUrl: "https://picsum.photos/200/300?random=17",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 18,
-  //   imgUrl: "https://picsum.photos/200/300?random=18",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 19,
-  //   imgUrl: "https://picsum.photos/200/300?random=19",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 20,
-  //   imgUrl: "https://picsum.photos/200/300?random=20",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 21,
-  //   imgUrl: "https://picsum.photos/200/300?random=21",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 22,
-  //   imgUrl: "https://picsum.photos/200/300?random=22",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 23,
-  //   imgUrl: "https://picsum.photos/200/300?random=23",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 24,
-  //   imgUrl: "https://picsum.photos/200/300?random=24",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 25,
-  //   imgUrl: "https://picsum.photos/200/300?random=25",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 26,
-  //   imgUrl: "https://picsum.photos/200/300?random=26",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 27,
-  //   imgUrl: "https://picsum.photos/200/300?random=27",
-  //   yearClicked: 2012,
-  // },
-  // {
-  //   id: 28,
-  //   imgUrl: "https://picsum.photos/200/300?random=28",
-  //   yearClicked: 2014,
-  // },
-  // {
-  //   id: 29,
-  //   imgUrl: "https://picsum.photos/200/300?random=29",
-  //   yearClicked: 2013,
-  // },
-  // {
-  //   id: 30,
-  //   imgUrl: "https://picsum.photos/200/300?random=30",
-  //   yearClicked: 2012,
-  // },
+  {
+    id: 11,
+    imgUrl: "https://picsum.photos/200/300?random=11",
+    yearClicked: 2013,
+  },
+  {
+    id: 12,
+    imgUrl: "https://picsum.photos/200/300?random=12",
+    yearClicked: 2012,
+  },
+  {
+    id: 13,
+    imgUrl: "https://picsum.photos/200/300?random=13",
+    yearClicked: 2014,
+  },
+  {
+    id: 14,
+    imgUrl: "https://picsum.photos/200/300?random=14",
+    yearClicked: 2013,
+  },
+  {
+    id: 15,
+    imgUrl: "https://picsum.photos/200/300?random=15",
+    yearClicked: 2012,
+  },
+  {
+    id: 16,
+    imgUrl: "https://picsum.photos/200/300?random=16",
+    yearClicked: 2014,
+  },
+  {
+    id: 17,
+    imgUrl: "https://picsum.photos/200/300?random=17",
+    yearClicked: 2013,
+  },
+  {
+    id: 18,
+    imgUrl: "https://picsum.photos/200/300?random=18",
+    yearClicked: 2012,
+  },
+  {
+    id: 19,
+    imgUrl: "https://picsum.photos/200/300?random=19",
+    yearClicked: 2014,
+  },
+  {
+    id: 20,
+    imgUrl: "https://picsum.photos/200/300?random=20",
+    yearClicked: 2013,
+  },
+  {
+    id: 21,
+    imgUrl: "https://picsum.photos/200/300?random=21",
+    yearClicked: 2012,
+  },
+  {
+    id: 22,
+    imgUrl: "https://picsum.photos/200/300?random=22",
+    yearClicked: 2014,
+  },
+  {
+    id: 23,
+    imgUrl: "https://picsum.photos/200/300?random=23",
+    yearClicked: 2013,
+  },
+  {
+    id: 24,
+    imgUrl: "https://picsum.photos/200/300?random=24",
+    yearClicked: 2012,
+  },
+  {
+    id: 25,
+    imgUrl: "https://picsum.photos/200/300?random=25",
+    yearClicked: 2014,
+  },
+  {
+    id: 26,
+    imgUrl: "https://picsum.photos/200/300?random=26",
+    yearClicked: 2013,
+  },
+  {
+    id: 27,
+    imgUrl: "https://picsum.photos/200/300?random=27",
+    yearClicked: 2012,
+  },
+  {
+    id: 28,
+    imgUrl: "https://picsum.photos/200/300?random=28",
+    yearClicked: 2014,
+  },
+  {
+    id: 29,
+    imgUrl: "https://picsum.photos/200/300?random=29",
+    yearClicked: 2013,
+  },
+  {
+    id: 30,
+    imgUrl: "https://picsum.photos/200/300?random=30",
+    yearClicked: 2012,
+  },
 ];
